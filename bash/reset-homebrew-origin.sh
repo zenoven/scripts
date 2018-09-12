4d531f4b8f7352a8ee8077abc2a924b624d94fbf
zenoven <zenoven@gmail.com>
2018-09-12 09:36:08 +0800

    add homebrew scripts

---
+++ bash/reset-homebrew-origin.sh
@@ -0,0 +1,7 @@
+# 重置brew.git:
+cd "$(brew --repo)"
+git remote set-url origin https://github.com/Homebrew/brew.git
+
+# 重置homebrew-core.git:
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://github.com/Homebrew/homebrew-core.git
---
+++ bash/update-homebrew-origin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# replaceHomebrewSoure.sh
+
+# 替换brew.git:
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+
+# 替换homebrew-core.git:
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
